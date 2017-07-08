a0b445e015bbd21e1c394fbf3ef13677fbbdd473
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-07-07 18:24:55 -0700

    changed SearchBar input into controlled input

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,10 +4,27 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux'
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {term: ''};
+
+        this.onInputChange = this.onInputChange.bind(this)
+    }
+
+    onInputChange(event){
+        this.setState({term: event.target.value});
+    }
+
     render(){
         return (
             <form className="input-group">
-                <input />
+                <input
+                    onChange={this.onInputChange}
+                    value={this.state.term}
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    className="form-control"
+                />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
