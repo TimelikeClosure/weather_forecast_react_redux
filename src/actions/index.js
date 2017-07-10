e0912b2995cf3c9e5687f895ac0579337219fa65
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-07-10 11:12:37 -0700

    added fetchWeather action
    added axios npm package
    added fetchWeather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
 "use strict";
+import axios from 'axios';
 
 const API_KEY = 'b992a5dc3a082b2df1ef71fa37c8f298';
+const BASE_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${BASE_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
